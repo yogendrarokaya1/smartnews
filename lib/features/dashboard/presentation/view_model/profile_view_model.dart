da429b1d6e142af477a802cf14c31dcfe19f8d46
yogendrarokaya1 <yogendrarokaya@gmail.com>
2026-01-31 21:13:36 +0545

    testing for login and signup

--- lib/features/dashboard/presentation/view_model/profile_view_model.dart
+++ lib/features/dashboard/presentation/view_model/profile_view_model.dart
@@ -1,40 +1,40 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../domain/usecases/profile_usecase.dart';
-import '../../domain/usecases/update_profile_usecase.dart';
-import 'profile_state.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import '../../domain/usecases/profile_usecase.dart';
+// import '../../domain/usecases/update_profile_usecase.dart';
+// import 'profile_state.dart';
 
-class ProfileViewModel extends StateNotifier<ProfileState> {
-  final GetProfileUseCase getProfileUseCase;
-  final UpdateProfileUseCase updateProfileUseCase;
+// class ProfileViewModel extends StateNotifier<ProfileState> {
+//   final GetProfileUseCase getProfileUseCase;
+//   final UpdateProfileUseCase updateProfileUseCase;
 
-  ProfileViewModel(this.getProfileUseCase, this.updateProfileUseCase)
-    : super(const ProfileState()) {
-    fetchProfile();
-  }
+//   ProfileViewModel(this.getProfileUseCase, this.updateProfileUseCase)
+//     : super(const ProfileState()) {
+//     fetchProfile();
+//   }
 
-  Future<void> fetchProfile() async {
-    state = state.copyWith(isLoading: true);
+//   Future<void> fetchProfile() async {
+//     state = state.copyWith(isLoading: true);
 
-    try {
-      final user = await getProfileUseCase();
-      state = state.copyWith(user: user, isLoading: false);
-    } catch (e) {
-      state = state.copyWith(error: e.toString(), isLoading: false);
-    }
-  }
+//     try {
+//       final user = await getProfileUseCase();
+//       state = state.copyWith(user: user, isLoading: false);
+//     } catch (e) {
+//       state = state.copyWith(error: e.toString(), isLoading: false);
+//     }
+//   }
 
-  Future<void> updateProfile({required String name, String? avatarPath}) async {
-    state = state.copyWith(isLoading: true);
+//   Future<void> updateProfile({required String name, String? avatarPath}) async {
+//     state = state.copyWith(isLoading: true);
 
-    try {
-      final user = await updateProfileUseCase(
-        name: name,
-        avatarPath: avatarPath,
-      );
+//     try {
+//       final user = await updateProfileUseCase(
+//         name: name,
+//         avatarPath: avatarPath,
+//       );
 
-      state = state.copyWith(user: user, isLoading: false);
-    } catch (e) {
-      state = state.copyWith(error: e.toString(), isLoading: false);
-    }
-  }
-}
+//       state = state.copyWith(user: user, isLoading: false);
+//     } catch (e) {
+//       state = state.copyWith(error: e.toString(), isLoading: false);
+//     }
+//   }
+// }
