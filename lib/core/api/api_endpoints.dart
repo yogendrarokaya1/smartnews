9ed1838d0e5b73bee66a105dd12afdaf192db98f
yogendrarokaya1 <yogendrarokaya@gmail.com>
2026-01-31 18:13:18 +0545

    testing setup

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -2,15 +2,19 @@ class ApiEndpoints {
   ApiEndpoints._();
 
   static const String baseUrl = 'http://10.0.2.2:5000';
+  // For Android Emulator use: 'http://10.0.2.2:3000/api/v1'
+  // For Physical Device use your computer's IP: 'http://192.168.x.x:5000/api/v1'
 
   static const Duration connectionTimeout = Duration(seconds: 30);
   static const Duration receiveTimeout = Duration(seconds: 30);
 
-  // ============ Auth ============
+  // ============ Auth Endpoints ============
   static const String register = '/api/auth/register';
   static const String login = '/api/auth/login';
+  static const String whoami = '/api/auth/whoami';
+  static const String updateprofile = '/api/auth/update-profile';
 
-  // ============ Profile ============
-  static const String getProfile = '/api/auth/whoami';
-  static const String updateProfile = '/api/auth/update-profile';
+  // ============ User Endpoints ============
+  static const String user = '/users';
+  static String userById(String id) => '/users/$id';
 }
