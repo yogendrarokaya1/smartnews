3e8101ad3f483bb059313e6e998292f5a235b700
yogendrarokaya1 <yogendrarokaya@gmail.com>
2026-01-31 17:23:07 +0545

    remote data source updated

--- lib/features/dashboard/data/datasources/remote/profile_remote_datasource_impl.dart
+++ lib/features/dashboard/data/datasources/remote/profile_remote_datasource_impl.dart
@@ -29,29 +29,29 @@
 //   }
 // }
 
-// import 'package:dio/dio.dart';
-// import 'package:smartnews/features/dashboard/data/datasources/remote/profile_remote_datasource_impl.dart'
-//     as ApiEndpoints;
-// import 'package:smartnews/features/dashboard/data/models/user_model.dart';
+import 'package:dio/dio.dart';
+import 'package:smartnews/features/dashboard/data/datasources/remote/profile_remote_datasource_impl.dart'
+    as ApiEndpoints;
+import 'package:smartnews/features/dashboard/data/models/user_model.dart';
 
-// @override
-// Future<UserModel> getProfile() async {
-//   final response = await dio.get(ApiEndpoints.getProfile);
-//   return UserModel.fromJson(response.data);
-// }
+@override
+Future<UserModel> getProfile() async {
+  final response = await dio.get(ApiEndpoints.getProfile);
+  return UserModel.fromJson(response.data);
+}
 
-// @override
-// Future<UserModel> updateProfile(String name, String? avatarPath) async {
-//   final data = {'name': name};
+@override
+Future<UserModel> updateProfile(String name, String? avatarPath) async {
+  final data = {'name': name};
 
-//   if (avatarPath != null) {
-//     data['avatar'] = (await MultipartFile.fromFile(avatarPath)) as String;
-//   }
+  if (avatarPath != null) {
+    data['avatar'] = (await MultipartFile.fromFile(avatarPath)) as String;
+  }
 
-//   final response = await dio.put(
-//     ApiEndpoints.updateProfile,
-//     data: FormData.fromMap(data),
-//   );
+  final response = await dio.put(
+    ApiEndpoints.updateProfile,
+    data: FormData.fromMap(data),
+  );
 
-//   return UserModel.fromJson(response.data);
-// }
+  return UserModel.fromJson(response.data);
+}
